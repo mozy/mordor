c2ca771b720eefd7c36b52f975854de7f09177fe
Cody Cutrer <cody@mozy.com>
2009-05-27 14:36:24 -0600

    Stream interface.

---
+++ mordor/common/streams/stream.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2009 - Decho Corp.
+
+#include "stream.h"
+
+size_t
+Stream::write(const char *sz)
+{
+    return write(sz, strlen(sz));
+}
+
+size_t
+Stream::write(const void *b, size_t len)
+{
+    Buffer buf;
+    buf.copyIn(b, len);
+    return write(&buf, len);
+}
+
+std::string
+Stream::getDelimited(char delim)
+{
+    size_t offset = findDelimited(delim);
+    if (offset == 0)
+        return std::string();
+    Buffer buf;
+    size_t readResult = read(&buf, offset);
+    assert(readResult == offset);
+    // Don't copyOut the delimiter itself
+    std::string result;
+    result.resize(readResult - 1);
+    buf.copyOut(const_cast<char*>(result.data()), readResult - 1);
+    return result;
+}
---
+++ mordor/common/streams/stream.h
@@ -0,0 +1,50 @@
+#ifndef __STREAM_H__
+#define __STREAM_H__
+// Copyright (c) 2009 - Decho Corp.
+
+#include <cassert>
+#include <string>
+
+#include "buffer.h"
+
+class Stream
+{
+public:
+    enum CloseType {
+        READ,
+        WRITE,
+        BOTH
+    };
+
+    enum Anchor {
+        BEGIN,
+        CURRENT,
+        END
+    };
+
+    virtual ~Stream() {}
+
+    virtual bool supportsRead() { return false; }
+    virtual bool supportsWrite() { return false; }
+    virtual bool supportsSeek() { return false; }
+    virtual bool supportsSize() { return false; }
+    virtual bool supportsTruncate() { return false; }
+
+    virtual void close(CloseType type = BOTH) {}
+    virtual size_t read(Buffer *b, size_t len) { assert(false); return 0; }
+    virtual size_t write(const Buffer *b, size_t len) { assert(false); return 0; }
+    virtual long long seek(long long offset, Anchor anchor) { assert(false); return 0ll; }
+    virtual long long size() { assert(false); return 0ll; }
+    virtual void truncate(long long size) { assert(false); }
+    virtual void flush() {}
+    virtual size_t findDelimited(char delim) { assert(false); return 0; }
+
+    // Convenience functions - do *not* implement in FilterStream, so that
+    // filters do not need to implement these
+    virtual size_t write(const void *b, size_t len);
+    size_t write(const char *sz);
+
+    std::string getDelimited(char delim = '\n');
+};
+
+#endif
