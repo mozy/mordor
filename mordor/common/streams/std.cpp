7dbc1c0ce906ba8b31d8bd04c665b4a26c2a4ee8
Cody Cutrer <cody@mozy.com>
2009-05-27 16:48:20 -0600

    Stdin/out/errStreams

---
+++ mordor/common/streams/std.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2009 - Decho Corp.
+
+#include "std.h"
+
+StdinStream::StdinStream()
+{
+#ifdef WINDOWS
+    HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+    if (hStdIn == INVALID_HANDLE_VALUE) {
+        // throwExceptionFromLastError();
+    }
+    if (hStdIn == NULL) {
+        // throwExceptionFromLastError(ERROR_FILE_NOT_FOUND);
+    }
+    init(hStdIn);
+#else
+    init(STDIN_FILENO, false);
+#endif
+}
+
+StdoutStream::StdoutStream()
+{
+#ifdef WINDOWS
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hStdOut == INVALID_HANDLE_VALUE) {
+        // throwExceptionFromLastError();
+    }
+    if (hStdOut == NULL) {
+        // throwExceptionFromLastError(ERROR_FILE_NOT_FOUND);
+    }
+    init(hStdOut);
+#else
+    init(STDOUT_FILENO, false);
+#endif
+}
+
+StderrStream::StderrStream()
+{
+#ifdef WINDOWS
+    HANDLE hStdErr = GetStdHandle(STD_ERROR_HANDLE);
+    if (hStdErr == INVALID_HANDLE_VALUE) {
+        // throwExceptionFromLastError();
+    }
+    if (hStdErr == NULL) {
+        // throwExceptionFromLastError(ERROR_FILE_NOT_FOUND);
+    }
+    init(hStdErr);
+#else
+    init(STDERR_FILENO, false);
+#endif
+}
---
+++ mordor/common/streams/std.h
@@ -0,0 +1,39 @@
+#ifndef __STD_H__
+#define __STD_H__
+// Copyright (c) 2009 - Decho Corp.
+
+#include "version.h"
+
+#ifdef WINDOWS
+#include "handle.h"
+typedef HandleStream NativeStream;
+#else
+#include "fd.h"
+typedef FDStream NativeStream;
+#endif
+
+class StdinStream : public NativeStream
+{
+public:
+    StdinStream();
+
+    bool supportsWrite() { return false; }
+};
+
+class StdoutStream : public NativeStream
+{
+public:
+    StdoutStream();
+
+    bool supportsRead() { return false; }
+};
+
+class StderrStream : public NativeStream
+{
+public:
+    StderrStream();
+
+    bool supportsRead() { return false; }
+};
+
+#endif
