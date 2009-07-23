f13aa1aa31789e41d4bbe72167e1036a7a65e93b
Cody Cutrer <cody@mozy.com>
2009-07-23 12:49:11 -0600

    Coroutine class.

---
+++ mordor/common/coroutine.h
@@ -0,0 +1,141 @@
+#ifndef __COROUTINE_H__
+#define __COROUTINE_H__
+// Copyright (c) 2009 - Decho Corp.
+
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "fiber.h"
+
+struct DummyVoid;
+
+template <class Result, class Arg = DummyVoid>
+class Coroutine : public boost::enable_shared_from_this<Coroutine<Result, Arg> >
+{
+public:
+    typedef boost::shared_ptr<Coroutine> ptr;
+    typedef boost::weak_ptr<Coroutine> weak_ptr;
+public:
+    Coroutine()
+    {
+        m_fiber = Fiber::ptr(new Fiber(boost::bind(&Coroutine::run, this)));
+    }
+
+    Coroutine(boost::function<Result (typename Coroutine<Result, Arg>::ptr, Arg)> dg)
+        : m_dg(dg)
+    {
+        m_fiber = Fiber::ptr(new Fiber(boost::bind(&Coroutine::run, this)));
+    }
+
+    void reset()
+    {
+        m_fiber->reset();
+    }
+
+    void reset(boost::function<Result (typename Coroutine<Result, Arg>::ptr, Arg)> dg)
+    {
+        m_fiber->reset();
+        m_dg = dg;        
+    }
+
+    Result call(Arg arg)
+    {
+        m_arg = arg;
+        m_fiber->call();
+        return m_result;
+    }
+
+    Arg yield(Result r)
+    {
+        m_result = r;
+        Fiber::yield();
+        return m_arg;
+    }
+/*
+    template <class OtherArg>
+    Arg yieldTo(typename Coroutine<Arg, OtherArg>::ptr other, OtherArg arg)
+    {
+        other->m_arg = arg;
+        other->m_fiber->yieldTo();
+        return m_result;
+    }
+*/
+    Fiber::State state()
+    {
+        return m_fiber->state();
+    }
+
+private:
+    void run()
+    {
+        m_result = m_dg(shared_from_this(), m_arg);
+    }
+
+private:
+    boost::function<Result (typename Coroutine<Result, Arg>::ptr, Arg)> m_dg;
+    Result m_result;
+    Arg m_arg;
+    Fiber::ptr m_fiber;    
+};
+
+template <class Result>
+class Coroutine<Result, DummyVoid> : public boost::enable_shared_from_this<Coroutine<Result> >
+{
+public:
+    typedef boost::shared_ptr<Coroutine> ptr;
+    typedef boost::weak_ptr<Coroutine> weak_ptr;
+public:
+    Coroutine()
+    {
+        m_fiber = Fiber::ptr(new Fiber(boost::bind(&Coroutine::run, this)));
+    }
+
+    Coroutine(boost::function<Result (typename Coroutine<Result>::ptr)> dg)
+        : m_dg(dg)
+    {
+        m_fiber = Fiber::ptr(new Fiber(boost::bind(&Coroutine::run, this)));
+    }
+
+    void reset()
+    {
+        m_fiber->reset();
+    }
+
+    void reset(boost::function<Result (typename Coroutine<Result>::ptr)> dg)
+    {
+        m_fiber->reset();
+        m_dg = dg;        
+    }
+
+    Result call()
+    {
+        m_fiber->call();
+        return m_result;
+    }
+
+    void yield(Result r)
+    {
+        m_result = r;
+        Fiber::yield();
+    }
+
+    Fiber::State state()
+    {
+        return m_fiber->state();
+    }
+
+private:
+    void run()
+    {
+        m_result = m_dg(shared_from_this());
+    }
+
+private:
+    boost::function<Result (typename Coroutine<Result>::ptr)> m_dg;
+    Result m_result;
+    Fiber::ptr m_fiber;    
+};
+
+#endif
---
+++ mordor/common/tests/coroutine.cpp
@@ -0,0 +1,80 @@
+// Copyright (c) 2009 - Decho Corp.
+
+#include "mordor/common/coroutine.h"
+#include "mordor/test/test.h"
+
+static int countTo5(Coroutine<int>::ptr self)
+{
+    self->yield(1);
+    self->yield(2);
+    self->yield(3);
+    self->yield(4);
+    return 5;
+}
+
+TEST_WITH_SUITE(Coroutine, basic)
+{
+    Fiber::ptr mainFiber(new Fiber());
+    Coroutine<int>::ptr coro(new Coroutine<int>(&countTo5));
+    int sequence = 0;
+    TEST_ASSERT_EQUAL(coro->state(), Fiber::INIT);
+    while (coro->state() != Fiber::TERM) {
+        TEST_ASSERT_EQUAL(coro->call(), ++sequence);
+    }
+    TEST_ASSERT_EQUAL(sequence, 5);
+}
+
+static int echo(Coroutine<int, int>::ptr self, int arg)
+{
+    while (arg < 5) {
+        arg = self->yield(arg);
+    }
+    return arg;
+}
+
+TEST_WITH_SUITE(Coroutine, basicWithArg)
+{
+    Fiber::ptr mainFiber(new Fiber());
+    Coroutine<int, int>::ptr coro(new Coroutine<int, int>(&echo));
+    TEST_ASSERT_EQUAL(coro->state(), Fiber::INIT);
+    for (int i = 0; i <= 5; ++i) {
+        TEST_ASSERT(coro->state() == Fiber::INIT || coro->state() == Fiber::HOLD);
+        TEST_ASSERT_EQUAL(coro->call(i), i);
+    }
+    TEST_ASSERT_EQUAL(coro->state(), Fiber::TERM);
+}
+/*
+static int producer(Coroutine<int, int>::ptr self, int arg,
+                    Coroutine<int, int>::weak_ptr weak_consumer)
+{
+    Coroutine<int, int>::ptr consumer(weak_consumer);
+    for (int i = 0; i < 5; ++i)
+        self->yieldTo(consumer, i);
+    return 0;
+}
+
+static int consumer(Coroutine<int, int>::ptr self, int arg,
+                    Coroutine<int, int>::weak_ptr weak_producer)
+{
+    Coroutine<int, int>::ptr producer(weak_producer);
+    int sequence = 0;
+    while (arg > 0) {
+        TEST_ASSERT_EQUAL(sequence++, arg);
+        arg = self->yieldTo(producer, 0);
+    }
+    return 0;
+}
+
+TEST_WITH_SUITE(Coroutine, producerConsumer)
+{
+    Fiber::ptr mainFiber(new Fiber());
+    Coroutine<int, int>::ptr producerCoro(new Coroutine<int, int>());
+    Coroutine<int, int>::ptr consumerCoro(new Coroutine<int, int>());
+
+    producerCoro->reset(boost::bind(&producer, _1, _2, Coroutine<int, int>::weak_ptr(consumerCoro)));
+    consumerCoro->reset(boost::bind(&consumer, _1, _2, Coroutine<int, int>::weak_ptr(producerCoro)));
+
+    producerCoro->call(0);
+    consumerCoro->call(-1);
+}
+*/
