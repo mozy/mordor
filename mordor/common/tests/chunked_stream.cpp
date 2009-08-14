89bac52e2e5291c8743a123509f7c095a7e4e6de
Cody Cutrer <cody@mozy.com>
2009-08-14 12:22:28 -0600

    ChunkedStream unit tests

---
+++ mordor/common/tests/chunked_stream.cpp
@@ -0,0 +1,128 @@
+// Copyright (c) 2009 - Decho Corp.
+
+#include "mordor/common/http/chunked.h"
+#include "mordor/common/streams/memory.h"
+#include "mordor/test/test.h"
+
+TEST_WITH_SUITE(ChunkedStream, readEmpty)
+{
+    Stream::ptr baseStream(new MemoryStream(Buffer("0\r\n")));
+    Stream::ptr chunkedStream(new HTTP::ChunkedStream(baseStream));
+
+    TEST_ASSERT(chunkedStream->supportsRead());
+    TEST_ASSERT(chunkedStream->supportsWrite());
+    TEST_ASSERT(!chunkedStream->supportsSeek());
+    TEST_ASSERT(!chunkedStream->supportsSize());
+    TEST_ASSERT(!chunkedStream->supportsTruncate());
+    TEST_ASSERT(!chunkedStream->supportsFind());
+    TEST_ASSERT(!chunkedStream->supportsUnread());
+
+    Buffer output;
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 10), 0u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 3);
+}
+
+TEST_WITH_SUITE(ChunkedStream, readSome)
+{
+    Stream::ptr baseStream(new MemoryStream(Buffer("a\r\nhelloworld\r\n0\r\n")));
+    Stream::ptr chunkedStream(new HTTP::ChunkedStream(baseStream));
+
+    Buffer output;
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 10u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 15);
+    TEST_ASSERT(output == "helloworld");
+
+    output.clear();
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 0u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 18);
+
+    output.clear();
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 0u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 18);
+}
+
+TEST_WITH_SUITE(ChunkedStream, readUppercaseHex)
+{
+    Stream::ptr baseStream(new MemoryStream(Buffer("A\r\nhelloworld\r\n0\r\n")));
+    Stream::ptr chunkedStream(new HTTP::ChunkedStream(baseStream));
+
+    Buffer output;
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 10u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 15);
+    TEST_ASSERT(output == "helloworld");
+}
+
+TEST_WITH_SUITE(ChunkedStream, ignoreExtensions)
+{
+    Stream::ptr baseStream(new MemoryStream(Buffer("a;mordor=rules\r\nhelloworld\r\n0\r\n")));
+    Stream::ptr chunkedStream(new HTTP::ChunkedStream(baseStream));
+
+    Buffer output;
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 10u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 28);
+    TEST_ASSERT(output == "helloworld");
+}
+
+TEST_WITH_SUITE(ChunkedStream, dontReadPastEnd)
+{
+    Stream::ptr baseStream(new MemoryStream(Buffer("a\r\nhelloworld\r\n0\r\nmore stuff")));
+    Stream::ptr chunkedStream(new HTTP::ChunkedStream(baseStream));
+
+    Buffer output;
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 10u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 15);
+    TEST_ASSERT(output == "helloworld");
+
+    output.clear();
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 0u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 18);
+
+    output.clear();
+    TEST_ASSERT_EQUAL(chunkedStream->read(output, 15), 0u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 18);
+
+    output.clear();
+    TEST_ASSERT_EQUAL(baseStream->read(output, 15), 10u);
+    TEST_ASSERT_EQUAL(baseStream->seek(0, Stream::CURRENT), 28);
+    TEST_ASSERT(output == "more stuff");
+}
+
+TEST_WITH_SUITE(ChunkedStream, writeEmpty)
+{
+    MemoryStream::ptr baseStream(new MemoryStream());
+    Stream::ptr chunkedStream(new HTTP::ChunkedStream(baseStream));
+
+    TEST_ASSERT(chunkedStream->supportsRead());
+    TEST_ASSERT(chunkedStream->supportsWrite());
+    TEST_ASSERT(!chunkedStream->supportsSeek());
+    TEST_ASSERT(!chunkedStream->supportsSize());
+    TEST_ASSERT(!chunkedStream->supportsTruncate());
+    TEST_ASSERT(!chunkedStream->supportsFind());
+    TEST_ASSERT(!chunkedStream->supportsUnread());
+
+    chunkedStream->close();
+    TEST_ASSERT(baseStream->buffer() == "0\r\n");
+}
+
+TEST_WITH_SUITE(ChunkedStream, write)
+{
+    MemoryStream::ptr baseStream(new MemoryStream());
+    Stream::ptr chunkedStream(new HTTP::ChunkedStream(baseStream));
+
+    TEST_ASSERT(chunkedStream->supportsRead());
+    TEST_ASSERT(chunkedStream->supportsWrite());
+    TEST_ASSERT(!chunkedStream->supportsSeek());
+    TEST_ASSERT(!chunkedStream->supportsSize());
+    TEST_ASSERT(!chunkedStream->supportsTruncate());
+    TEST_ASSERT(!chunkedStream->supportsFind());
+    TEST_ASSERT(!chunkedStream->supportsUnread());
+
+    TEST_ASSERT_EQUAL(chunkedStream->write("hello"), 5u);
+    TEST_ASSERT(baseStream->buffer() == "5\r\nhello\r\n");
+
+    TEST_ASSERT_EQUAL(chunkedStream->write("helloworld"), 10u);
+    TEST_ASSERT(baseStream->buffer() == "5\r\nhello\r\na\r\nhelloworld\r\n");
+
+    chunkedStream->close();
+    TEST_ASSERT(baseStream->buffer() == "5\r\nhello\r\na\r\nhelloworld\r\n0\r\n");
+}
