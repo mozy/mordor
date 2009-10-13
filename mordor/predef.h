da75b62e0d2e364c77e45442918a6bee6d3f20d6
Cody Cutrer <cody@mozy.com>
2009-10-13 15:01:13 -0600

    Add predef header

---
+++ mordor/predef.h
@@ -0,0 +1,43 @@
+#ifndef __MORDOR_PREDEF_H__
+#define __MORDOR_PREDEF_H__
+
+#include "version.h"
+
+#ifdef WINDOWS
+#define _WIN32_WINNT 0x0600
+#define WIN32_LEAN_AND_MEAN
+#define SECURITY_WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#define strtoull _strtoui64
+#define strnicmp _strnicmp
+
+#include <windows.h>
+
+#undef min
+#undef max
+#undef ABSOLUTE
+#undef RELATIVE
+#undef ERROR
+
+// Take things out of the preprocessor, and put into the global namespace
+#undef DELETE
+enum {
+    DELETE = (0x00010000L)
+};
+
+#else
+
+#ifdef major
+#undef major
+#undef minor
+#endif
+
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
+
+#endif
+
+
+#endif
