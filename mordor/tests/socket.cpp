4ce01a7b27c7f77fb88b62858635df42da7b14df
Cody Cutrer <cody@mozy.com>
2009-10-23 10:54:26 -0600

    Refactor Socket unit tests, and add more
     * Consolidate address lookup into a single function
     * Add send/receive test where everything happens synchronously
     * Add send/receive test where we can guarantee that every socket call "blocked"
    
    Change-Id: Ieaac493d0e15dea3c984f844fb8e17b8dd7ed287
    Reviewed-on: http://gerrit.dechocorp.com/1775
    Reviewed-by: Cody Cutrer <cody@mozy.com>

--- mordor/tests/socket.cpp
+++ mordor/tests/socket.cpp
@@ -12,6 +12,32 @@
 using namespace Mordor;
 using namespace Mordor::Test;
 
+namespace {
+struct Connection
+{
+    Socket::ptr connect;
+    Socket::ptr listen;
+    Address::ptr address;
+};
+}
+
+Connection
+establishConn(IOManager &ioManager)
+{
+    Connection result;
+    std::vector<Address::ptr> addresses = Address::lookup("localhost:8000", AF_UNSPEC, SOCK_STREAM);
+    MORDOR_TEST_ASSERT(!addresses.empty());
+    // TODO: random port
+    result.address = addresses.front();
+    result.listen = result.address->createSocket(ioManager);
+    unsigned int opt = 1;
+    result.listen->setOption(SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    result.listen->bind(addresses.front());
+    result.listen->listen();
+    result.connect = result.address->createSocket(ioManager);
+    return result;
+}
+
 MORDOR_SUITE_INVARIANT(Socket)
 {
     MORDOR_TEST_ASSERT(!Fiber::getThis());
@@ -22,17 +48,10 @@ MORDOR_UNITTEST(Socket, acceptTimeout)
 {
     Fiber::ptr mainfiber(new Fiber());
     IOManager ioManager;
-    std::vector<Address::ptr> addresses = Address::lookup("localhost:8000", AF_UNSPEC, SOCK_STREAM);
-    MORDOR_TEST_ASSERT(!addresses.empty());
-    // TODO: random port
-    Socket::ptr listen = addresses.front()->createSocket(ioManager);
-    listen->receiveTimeout(1000000);
-    unsigned int opt = 1;
-    listen->setOption(SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    listen->bind(addresses.front());
-    listen->listen();
+    Connection conns = establishConn(ioManager);
+    conns.listen->receiveTimeout(1000000);
     unsigned long long start = TimerManager::now();
-    MORDOR_TEST_ASSERT_EXCEPTION(listen->accept(), TimedOutException);
+    MORDOR_TEST_ASSERT_EXCEPTION(conns.listen->accept(), TimedOutException);
     MORDOR_TEST_ASSERT_ABOUT_EQUAL(start + 1000000, TimerManager::now(), 100000);
 }
 
@@ -40,20 +59,12 @@ MORDOR_UNITTEST(Socket, receiveTimeout)
 {
     Fiber::ptr mainfiber(new Fiber());
     IOManager ioManager;
-    std::vector<Address::ptr> addresses = Address::lookup("localhost:8000", AF_UNSPEC, SOCK_STREAM);
-    MORDOR_TEST_ASSERT(!addresses.empty());
-    // TODO: random port
-    Socket::ptr listen = addresses.front()->createSocket(ioManager);
-    unsigned int opt = 1;
-    listen->setOption(SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    listen->bind(addresses.front());
-    listen->listen();
-    Socket::ptr connect = addresses.front()->createSocket(ioManager);
-    connect->receiveTimeout(1000000);
-    connect->connect(addresses.front());
+    Connection conns = establishConn(ioManager);
+    conns.connect->receiveTimeout(1000000);
+    conns.connect->connect(conns.address);
     char buf;
     unsigned long long start = TimerManager::now();
-    MORDOR_TEST_ASSERT_EXCEPTION(connect->receive(&buf, 1), TimedOutException);
+    MORDOR_TEST_ASSERT_EXCEPTION(conns.connect->receive(&buf, 1), TimedOutException);
     MORDOR_TEST_ASSERT_ABOUT_EQUAL(start + 1000000, TimerManager::now(), 100000);
 }
 
@@ -65,19 +76,11 @@ static void testShutdownException(bool send, bool shutdown, bool otherEnd)
 {
     Fiber::ptr mainfiber(new Fiber());
     IOManager ioManager;
-    std::vector<Address::ptr> addresses = Address::lookup("localhost:8000", AF_UNSPEC, SOCK_STREAM);
-    MORDOR_TEST_ASSERT(!addresses.empty());
-    // TODO: random port
-    Socket::ptr listen = addresses.front()->createSocket(ioManager);
-    unsigned int opt = 1;
-    listen->setOption(SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    listen->bind(addresses.front());
-    listen->listen();
-    Socket::ptr connect = addresses.front()->createSocket(ioManager);
-    connect->connect(addresses.front());
-    Socket::ptr accept = listen->accept();
+    Connection conns = establishConn(ioManager);
+    conns.connect->connect(conns.address);
+    Socket::ptr accept = conns.listen->accept();
 
-    Socket::ptr socketToClose = otherEnd ? accept : connect;
+    Socket::ptr socketToClose = otherEnd ? accept : conns.connect;
     if (shutdown)
         socketToClose->shutdown(SHUT_RDWR);
     else
@@ -86,28 +89,28 @@ static void testShutdownException(bool send, bool shutdown, bool otherEnd)
     if (send) {
         if (otherEnd) {
             try {
-                connect->sendTimeout(100);
+                conns.connect->sendTimeout(100);
                 while (true) {
-                    MORDOR_TEST_ASSERT_EQUAL(connect->send("abc", 3), 3u);
+                    MORDOR_TEST_ASSERT_EQUAL(conns.connect->send("abc", 3), 3u);
                 }
             } catch (Exception) {
             }
         } else {
-            MORDOR_TEST_ASSERT_EXCEPTION(connect->send("abc", 3), Exception);
+            MORDOR_TEST_ASSERT_EXCEPTION(conns.connect->send("abc", 3), Exception);
         }
     } else {
         unsigned char readBuf[3];
         if (otherEnd) {
-            MORDOR_TEST_ASSERT_EQUAL(connect->receive(readBuf, 3), 0u);
+            MORDOR_TEST_ASSERT_EQUAL(conns.connect->receive(readBuf, 3), 0u);
         } else {
 #ifndef WINDOWS
             // Silly non-Windows letting you receive after you told it no more
             if (shutdown) {
-                MORDOR_TEST_ASSERT_EQUAL(connect->receive(readBuf, 3), 0u);
+                MORDOR_TEST_ASSERT_EQUAL(conns.connect->receive(readBuf, 3), 0u);
             } else
 #endif
             {
-                MORDOR_TEST_ASSERT_EXCEPTION(connect->receive(readBuf, 3), Exception);
+                MORDOR_TEST_ASSERT_EXCEPTION(conns.connect->receive(readBuf, 3), Exception);
             }
         }
     }
@@ -202,16 +205,180 @@ MORDOR_UNITTEST(Socket, cancelAccept)
 {
     Fiber::ptr mainfiber(new Fiber());
     IOManager ioManager;
-    std::vector<Address::ptr> addresses = Address::lookup("localhost:8000", AF_UNSPEC, SOCK_STREAM);
-    MORDOR_TEST_ASSERT(!addresses.empty());
-    // TODO: random port
-    Socket::ptr listen = addresses.front()->createSocket(ioManager);
-    unsigned int opt = 1;
-    listen->setOption(SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    listen->bind(addresses.front());
-    listen->listen();
+    Connection conns = establishConn(ioManager);
 
     // cancelMe will get run when this fiber yields because it would block
-    ioManager.schedule(Fiber::ptr(new Fiber(boost::bind(&cancelMe, listen))));
-    MORDOR_TEST_ASSERT_EXCEPTION(listen->accept(), OperationAbortedException);
+    ioManager.schedule(Fiber::ptr(new Fiber(boost::bind(&cancelMe, conns.listen))));
+    MORDOR_TEST_ASSERT_EXCEPTION(conns.listen->accept(), OperationAbortedException);
+}
+
+MORDOR_UNITTEST(Socket, sendReceive)
+{
+    Fiber::ptr mainfiber(new Fiber());
+    IOManager ioManager;
+    Connection conns = establishConn(ioManager);
+    conns.connect->connect(conns.address);
+    Socket::ptr accept = conns.listen->accept();
+
+    const char *sendbuf = "abcd";
+    char receivebuf[5];
+    memset(receivebuf, 0, 5);
+    MORDOR_TEST_ASSERT_EQUAL(conns.connect->send(sendbuf, 1), 1u);
+    MORDOR_TEST_ASSERT_EQUAL(accept->receive(receivebuf, 1), 1u);
+    MORDOR_TEST_ASSERT_EQUAL(receivebuf[0], 'a');
+    receivebuf[0] = 0;
+    iovec iov[2];
+    iov[0].iov_base = (void *)&sendbuf[0];
+    iov[1].iov_base = (void *)&sendbuf[2];
+    iov[0].iov_len = 2;
+    iov[1].iov_len = 2;
+    MORDOR_TEST_ASSERT_EQUAL(conns.connect->send(iov, 2), 4u);
+    iov[0].iov_base = &receivebuf[0];
+    iov[1].iov_base = &receivebuf[2];
+    MORDOR_TEST_ASSERT_EQUAL(accept->receive(iov, 2), 4u);
+    MORDOR_TEST_ASSERT_EQUAL(sendbuf, (const char *)receivebuf);
+}
+
+static void receiveFiber(Socket::ptr listen, size_t &sent, int &sequence)
+{
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 1);
+    Socket::ptr sock = listen->accept();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 3);
+    char receivebuf[5];
+    memset(receivebuf, 0, 5);
+    Scheduler::getThis()->yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 5);
+    MORDOR_TEST_ASSERT_EQUAL(sock->receive(receivebuf, 2), 2u);
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 7);
+    MORDOR_TEST_ASSERT_EQUAL((const char*)receivebuf, "ab");
+    memset(receivebuf, 0, 5);
+    iovec iov[2];
+    iov[0].iov_base = &receivebuf[0];
+    iov[1].iov_base = &receivebuf[2];
+    iov[0].iov_len = 2;
+    iov[1].iov_len = 2;
+    Scheduler::getThis()->yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 9);
+    MORDOR_TEST_ASSERT_EQUAL(sock->receive(iov, 2), 4u);
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 11);
+    MORDOR_TEST_ASSERT_EQUAL((const char*)receivebuf, "abcd");
+
+    // Let the main fiber take control again until he "blocks"
+    Scheduler::getThis()->yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 13);
+    boost::shared_array<char> receivebuf2;
+    if (sent > 0u) {
+        receivebuf2.reset(new char[sent]);
+        while (sent > 0u)
+            sent -= sock->receive(receivebuf2.get(), sent);
+    }
+    // Let the main fiber update sent
+    Scheduler::getThis()->yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 15);
+
+    if (sent > 0u) {
+        receivebuf2.reset(new char[sent]);
+        while (sent > 0u)
+            sent -= sock->receive(receivebuf2.get(), sent);
+    }
+
+    // Let the main fiber take control again until he "blocks"
+    Scheduler::getThis()->yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 17);
+    if (sent > 0u) {
+        receivebuf2.reset(new char[std::max<size_t>(sent, 3u)]);
+        iov[0].iov_base = &receivebuf2.get()[0];
+        iov[1].iov_base = &receivebuf2.get()[2];
+        iov[1].iov_len = std::max<size_t>(sent, 3u) - 2;
+        while (sent > 0u)
+            sent -= sock->receive(iov, 2);
+    }
+    // Let the main fiber update sent
+    Scheduler::getThis()->yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 19);
+
+    if (sent > 0u) {
+        receivebuf2.reset(new char[std::max<size_t>(sent, 3u)]);
+        iov[0].iov_base = &receivebuf2.get()[0];
+        iov[1].iov_base = &receivebuf2.get()[2];
+        iov[1].iov_len = std::max<size_t>(sent, 3u) - 2;
+        while (sent > 0u)
+            sent -= sock->receive(iov, 2);
+    }
+}
+
+MORDOR_UNITTEST(Socket, sendReceiveForceAsync)
+{
+    Fiber::ptr mainfiber(new Fiber());
+    IOManager ioManager;
+    Connection conns = establishConn(ioManager);
+    int sequence = 0;
+    size_t sent = 0;
+
+    Fiber::ptr otherfiber(new Fiber(boost::bind(&receiveFiber, conns.listen,
+        boost::ref(sent), boost::ref(sequence))));
+
+    ioManager.schedule(otherfiber);
+    // Wait for otherfiber to "block", and return control to us
+    ioManager.schedule(Fiber::getThis());
+    ioManager.yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 2);
+    // Unblock him, then wait for him to block again
+    conns.connect->connect(conns.address);
+    ioManager.dispatch();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 4);
+    ioManager.schedule(otherfiber);
+    ioManager.schedule(Fiber::getThis());
+    ioManager.yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 6);
+
+    // Again
+    const char *sendbuf = "abcd";
+    MORDOR_TEST_ASSERT_EQUAL(conns.connect->send(sendbuf, 2), 2u);
+    ioManager.dispatch();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 8);
+    ioManager.schedule(otherfiber);
+    ioManager.schedule(Fiber::getThis());
+    ioManager.yieldTo();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 10);
+    
+    // And again
+    iovec iov[2];
+    iov[0].iov_base = (void *)&sendbuf[0];
+    iov[1].iov_base = (void *)&sendbuf[2];
+    iov[0].iov_len = 2;
+    iov[1].iov_len = 2;
+    MORDOR_TEST_ASSERT_EQUAL(conns.connect->send(iov, 2), 4u);
+    ioManager.dispatch();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 12);
+
+    char sendbuf2[65536];
+    memset(sendbuf2, 1, 65536);
+    // Keep sending until the other fiber said we blocked
+    ioManager.schedule(otherfiber);
+    while (true) {
+        sent += conns.connect->send(sendbuf2, 65536);
+        if (sequence == 13)
+            break;
+    }
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 14);
+    ioManager.schedule(otherfiber);
+    ioManager.dispatch();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 16);
+    MORDOR_TEST_ASSERT_EQUAL(sent, 0u);
+
+    iov[0].iov_base = &sendbuf2[0];
+    iov[1].iov_base = &sendbuf2[2];
+    iov[1].iov_len = 65536 - 2;
+    // Keep sending until the other fiber said we blocked
+    ioManager.schedule(otherfiber);
+    while (true) {
+        sent += conns.connect->send(iov, 2);
+        if (sequence == 17)
+            break;
+    }
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 18);
+    ioManager.schedule(otherfiber);
+    ioManager.dispatch();
+    MORDOR_TEST_ASSERT_EQUAL(++sequence, 20);    
 }
