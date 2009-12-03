6449bcd939156a5ee5908222de94f91efb395659
Cody Cutrer <cody@mozy.com>
2009-12-03 13:23:39 -0700

    Simplify interface for transferStream.
    Change-Id: I597509fb26ff485fd3fe9aa1662aaf5a363154b1
    Reviewed-on: http://gerrit.dechocorp.com/2559
    Reviewed-by: Jeremy Stanley <jeremy@mozy.com>

--- mordor/streams/transfer.cpp
+++ mordor/streams/transfer.cpp
@@ -18,21 +18,17 @@ static void readOne(Stream &src, Buffer *&buffer, size_t len, size_t &result)
     result = src.read(*buffer, len);
 }
 
-static void writeOne(Stream &dst, Buffer *&buffer, unsigned long long *totalWritten)
+static void writeOne(Stream &dst, Buffer *&buffer)
 {
     size_t result;
     while (buffer->readAvailable() > 0) {
         result = dst.write(*buffer, buffer->readAvailable());
         buffer->consume(result);
-        if (totalWritten)
-            *totalWritten += result;
     }
 }
 
-void transferStream(Stream &src, Stream &dst,
-                            unsigned long long toTransfer,
-                            unsigned long long *totalRead,
-                            unsigned long long *totalWritten)
+unsigned long long transferStream(Stream &src, Stream &dst,
+                                  unsigned long long toTransfer)
 {
     MORDOR_ASSERT(src.supportsRead());
     MORDOR_ASSERT(dst.supportsWrite());
@@ -41,40 +37,34 @@ void transferStream(Stream &src, Stream &dst,
     size_t chunkSize = 65536;
     size_t todo;
     size_t readResult;
-    unsigned long long totalReadS;
-    if (!totalRead)
-        totalRead = &totalReadS;
-    *totalRead = 0;
-    if (totalWritten)
-        *totalWritten = 0;
+    unsigned long long totalRead = 0;
     if (toTransfer == 0)
-        return;
+        return 0;
 
     readBuffer = &buf1;
     todo = chunkSize;
-    if (toTransfer - *totalRead < (unsigned long long)todo)
-        todo = (size_t)(toTransfer - *totalRead);
+    if (toTransfer - totalRead < (unsigned long long)todo)
+        todo = (size_t)(toTransfer - totalRead);
     readOne(src, readBuffer, todo, readResult);
-    *totalRead += readResult;
-    if (readResult == 0 && toTransfer != ~0ull) {
+    totalRead += readResult;
+    if (readResult == 0 && toTransfer != ~0ull)
         MORDOR_THROW_EXCEPTION(UnexpectedEofException());
-    }
     if (readResult == 0)
-        return;
+        return totalRead;
 
     // Optimize transfer to NullStream
     if (&dst == &NullStream::get()) {
         while (true) {
             readBuffer->clear();
             todo = chunkSize;
-            if (toTransfer - *totalRead < (unsigned long long)todo)
-                todo = (size_t)(toTransfer - *totalRead);
+            if (toTransfer - totalRead < (unsigned long long)todo)
+                todo = (size_t)(toTransfer - totalRead);
             readOne(src, readBuffer, todo, readResult);
-            *totalRead += readResult;
+            totalRead += readResult;
             if (readResult == 0 && toTransfer != ~0ull)
                 MORDOR_THROW_EXCEPTION(UnexpectedEofException());
             if (readResult == 0)
-                return;
+                return totalRead;
         }
     }
 
@@ -86,60 +76,26 @@ void transferStream(Stream &src, Stream &dst,
     fibers[1].reset(new Fiber(NULL));
     dgs[0] = boost::bind(&readOne, boost::ref(src), boost::ref(readBuffer),
         boost::ref(todo), boost::ref(readResult));
-    dgs[1] = boost::bind(&writeOne, boost::ref(dst), boost::ref(writeBuffer), 
-        totalWritten);
-    while (*totalRead < toTransfer) {
+    dgs[1] = boost::bind(&writeOne, boost::ref(dst), boost::ref(writeBuffer));
+    while (totalRead < toTransfer) {
         writeBuffer = readBuffer;
         if (readBuffer == &buf1)
             readBuffer = &buf2;
         else
             readBuffer = &buf1;
         todo = chunkSize;
-        if (toTransfer - *totalRead < (unsigned long long)todo)
-            todo = (size_t)(toTransfer - *totalRead);
+        if (toTransfer - totalRead < (unsigned long long)todo)
+            todo = (size_t)(toTransfer - totalRead);
         parallel_do(dgs, fibers);
-        *totalRead += readResult;
-        if (readResult == 0 && toTransfer != ~0ull && *totalRead < toTransfer) {
+        totalRead += readResult;
+        if (readResult == 0 && toTransfer != ~0ull && totalRead < toTransfer)
             MORDOR_THROW_EXCEPTION(UnexpectedEofException());
-        }
         if (readResult == 0)
-            return;
+            return totalRead;
     }
     writeBuffer = readBuffer;
-    writeOne(dst, writeBuffer, totalWritten);
+    writeOne(dst, writeBuffer);
+    return totalRead;
 }
-
-void transferStream(Stream::ptr src, Stream &dst,
-                    unsigned long long toTransfer,
-                    unsigned long long *totalRead,
-                    unsigned long long *totalWritten)
-{ transferStream(*src.get(), dst, toTransfer, totalRead, totalWritten); }
-void transferStream(Stream &src, Stream::ptr dst,
-                    unsigned long long toTransfer,
-                    unsigned long long *totalRead,
-                    unsigned long long *totalWritten)
-{ transferStream(src, *dst.get(), toTransfer, totalRead, totalWritten); }
-void transferStream(Stream::ptr src, Stream::ptr dst,
-                    unsigned long long toTransfer,
-                    unsigned long long *totalRead,
-                    unsigned long long *totalWritten)
-{ transferStream(*src.get(), *dst.get(), toTransfer, totalRead, totalWritten); }
-
-void transferStream(Stream &src, Stream &dst,
-                    unsigned long long *totalRead,
-                    unsigned long long *totalWritten)
-{ transferStream(src, dst, ~0, totalRead, totalWritten); }
-void transferStream(Stream::ptr src, Stream &dst,
-                    unsigned long long *totalRead,
-                    unsigned long long *totalWritten)
-{ transferStream(*src.get(), dst, ~0, totalRead, totalWritten); }
-void transferStream(Stream &src, Stream::ptr dst,
-                    unsigned long long *totalRead,
-                    unsigned long long *totalWritten)
-{ transferStream(src, *dst.get(), ~0, totalRead, totalWritten); }
-void transferStream(Stream::ptr src, Stream::ptr dst,
-                    unsigned long long *totalRead,
-                    unsigned long long *totalWritten)
-{ transferStream(*src.get(), *dst.get(), ~0, totalRead, totalWritten); }
 
 }
--- mordor/streams/transfer.h
+++ mordor/streams/transfer.h
@@ -6,24 +6,18 @@
 
 namespace Mordor {
 
-void transferStream(Stream& src, Stream& dst, unsigned long long toTransfer,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
+unsigned long long transferStream(Stream &src, Stream &dst,
+                                  unsigned long long toTransfer = ~0ull);
 
-void transferStream(Stream::ptr src, Stream& dst, unsigned long long toTransfer,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
-void transferStream(Stream &src, Stream::ptr dst, unsigned long long toTransfer,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
-void transferStream(Stream::ptr src, Stream::ptr dst, unsigned long long toTransfer,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
-
-void transferStream(Stream &src, Stream &dst,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
-void transferStream(Stream::ptr src, Stream &dst,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
-void transferStream(Stream &src, Stream::ptr dst,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
-void transferStream(Stream::ptr src, Stream::ptr dst,
-                    unsigned long long *totalRead = 0, unsigned long long *totalWritten = 0);
+inline unsigned long long transferStream(Stream::ptr src, Stream &dst,
+                                         unsigned long long toTransfer = ~0ull)
+{ return transferStream(*src.get(), dst, toTransfer); }
+inline unsigned long long transferStream(Stream &src, Stream::ptr dst,
+                                         unsigned long long toTransfer = ~0ull)
+{ return transferStream(src, *dst.get(), toTransfer); }
+inline unsigned long long transferStream(Stream::ptr src, Stream::ptr dst,
+                                         unsigned long long toTransfer = ~0ull)
+{ return transferStream(*src.get(), *dst.get(), toTransfer); }
 
 }
 
