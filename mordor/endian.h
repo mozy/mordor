bb2ab578477b26c1006d606981eac275d7c4406d
Cody Cutrer <cody@mozy.com>
2010-02-19 09:25:26 -0700

    Make ntohll an inline function instead of a #define, to avoid conflicts with other projects
    Change-Id: Ida6f70c6844749cb91e45a437040e8d0ea0ba84e
    Reviewed-on: https://gerrit.dechocorp.com/4568
    Reviewed-by: Brian Palmer <brian@mozy.com>

--- mordor/endian.h
+++ mordor/endian.h
@@ -4,53 +4,75 @@
 
 #include "version.h"
 
+namespace Mordor {
+
 #ifndef ntohll
 
 #ifdef WINDOWS
 
 #include <stdlib.h>
 
-#define ntohll(x) _byteswap_uint64(x)
-#define htonll(x) _byteswap_uint64(x)
+inline unsigned __int64 ntohll(unsigned __int64 val)
+{ return _byteswap_uint64(val); }
+inline unsigned __int64 htonll(unsigned __int64 val)
+{ return _byteswap_uint64(val); }
 
 #elif defined(OSX)
 
+#include <stdint.h>
 #include <sys/_endian.h>
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-#define ntohll(x) _OSSwapInt64(x)
-#define htonll(x) _OSSwapInt64(x)
+
+inline uint64_t ntohll(uint64_t val)
+{ return _OSSwapInt64(val); }
+inline uint64_t htonll(uint64_t val)
+{ return _OSSwapInt64(val); }
 #else
-#define ntohll(x) (x)
-#define htonll(x) (x)
+inline uint64_t ntohll(uint64_t val)
+{ return val; }
+inline uint64_t htonll(uint64_t val)
+{ return val; }
 #endif
 
 #elif defined(FREEBSD)
 
+#include <stdint.h>
 #include <sys/endian.h>
 
 #if _BYTE_ORDER == _LITTLE_ENDIAN
-#define ntohll(x) bswap64(x)
-#define htonll(x) bswap64(x)
+inline uint64_t ntohll(uint64_t val)
+{ return bswap64(val); }
+inline uint64_t htonll(uint64_t val)
+{ return bswap64(val); }
 #else
-#define ntohll(x) (x)
-#define htonll(x) (x)
+inline uint64_t ntohll(uint64_t val)
+{ return val; }
+inline uint64_t htonll(uint64_t val)
+{ return val; }
 #endif
 
 #else
 
 #include <byteswap.h>
+#include <stdint.h>
 
 #if BYTE_ORDER == LITTLE_ENDIAN
-#define ntohll(x) bswap_64(x)
-#define htonll(x) bswap_64(x)
+inline uint64_t ntohll(uint64_t val)
+{ return bswap_64(val); }
+inline uint64_t htonll(uint64_t val)
+{ return bswap_64(val); }
 #else
-#define ntohll(x) (x)
-#define htonll(x) (x)
+inline uint64_t ntohll(uint64_t val)
+{ return val; }
+inline uint64_t htonll(uint64_t val)
+{ return val; }
 #endif
 
 #endif
 
 #endif
+
+}
 
 #endif
