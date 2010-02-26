0d7fb734e269c8d8f23d56a9d5dd8c2bea461051
Cody Cutrer <cody@mozy.com>
2010-02-26 16:12:48 -0700

    TimeoutStream fixes
     * Be sure to cancel the timeout if an exception happens *besides* OperationAbortedException.
     * Don't keep a reference around to a timer that's been cancelled, because it can't be reset (though we'll try).
    
    Change-Id: I9f70334da1de9cdf35545100e4d2c9e1b7606d10
    Reviewed-on: https://gerrit.dechocorp.com/4810
    Reviewed-by: Russ Simons <russs@decho.com>

--- mordor/streams/timeout.cpp
+++ mordor/streams/timeout.cpp
@@ -33,10 +33,12 @@ TimeoutStream::readTimeout(unsigned long long readTimeout)
     FiberMutex::ScopedLock lock(m_mutex);
     m_readTimeout = readTimeout;
     if (m_readTimer) {
-        if (readTimeout == ~0ull)
+        if (readTimeout == ~0ull) {
             m_readTimer->cancel();
-        else
+            m_readTimer.reset();
+        } else {
             m_readTimer->reset(readTimeout, true);
+        }
     } else if (m_readTimeout != ~0ull && !m_readTimedOut) {
         m_readTimer = m_timerManager.registerTimer(m_readTimeout,
             boost::bind(&cancelReadLocal, parent(),
@@ -50,10 +52,12 @@ TimeoutStream::writeTimeout(unsigned long long writeTimeout)
     FiberMutex::ScopedLock lock(m_mutex);
     m_writeTimeout = writeTimeout;
     if (m_writeTimer) {
-        if (writeTimeout == ~0ull)
+        if (writeTimeout == ~0ull) {
             m_writeTimer->cancel();
-        else
+            m_writeTimer.reset();
+        } else {
             m_writeTimer->reset(writeTimeout, true);
+        }
     } else if (m_writeTimeout != ~0ull && !m_writeTimedOut) {
         m_writeTimer = m_timerManager.registerTimer(m_writeTimeout,
             boost::bind(&cancelWriteLocal, parent(),
@@ -66,6 +70,7 @@ TimeoutStream::read(Buffer &buffer, size_t length)
 {
     FiberMutex::ScopedLock lock(m_mutex);
     m_readTimedOut = false;
+    MORDOR_ASSERT(!m_readTimer);
     if (m_readTimeout != ~0ull)
         m_readTimer = m_timerManager.registerTimer(m_readTimeout,
             boost::bind(&cancelReadLocal, parent(),
@@ -76,16 +81,28 @@ TimeoutStream::read(Buffer &buffer, size_t length)
         result = parent()->read(buffer, length);
     } catch (OperationAbortedException &) {
         lock.lock();
-        if (m_readTimer)
+        if (m_readTimer) {
             m_readTimer->cancel();
+            m_readTimer.reset();
+        }
         if (m_readTimedOut)
             MORDOR_THROW_EXCEPTION(TimedOutException());
         m_readTimedOut = true;
         throw;
+    } catch (...) {
+        lock.lock();
+        if (m_readTimer) {
+            m_readTimer->cancel();
+            m_readTimer.reset();
+        }
+        m_readTimedOut = true;
+        throw;
     }
     lock.lock();
-    if (m_readTimer)
+    if (m_readTimer) {
         m_readTimer->cancel();
+        m_readTimer.reset();
+    }
     m_readTimedOut = true;
     return result;
 }
@@ -95,6 +112,7 @@ TimeoutStream::write(const Buffer &buffer, size_t length)
 {
     FiberMutex::ScopedLock lock(m_mutex);
     m_writeTimedOut = false;
+    MORDOR_ASSERT(!m_writeTimer);
     if (m_writeTimeout != ~0ull)
         m_writeTimer = m_timerManager.registerTimer(m_writeTimeout,
             boost::bind(&cancelWriteLocal, parent(),
@@ -105,16 +123,28 @@ TimeoutStream::write(const Buffer &buffer, size_t length)
         result = parent()->write(buffer, length);
     } catch (OperationAbortedException &) {
         lock.lock();
-        if (m_writeTimer)
+        if (m_writeTimer) {
             m_writeTimer->cancel();
+            m_writeTimer.reset();
+        }
         if (m_writeTimedOut)
             MORDOR_THROW_EXCEPTION(TimedOutException());
         m_writeTimedOut = true;
         throw;
+    } catch (...) {
+        lock.lock();
+        if (m_writeTimer) {
+            m_writeTimer->cancel();
+            m_writeTimer.reset();
+        }
+        m_writeTimedOut = true;
+        throw;
     }
     lock.lock();
-    if (m_writeTimer)
+    if (m_writeTimer) {
         m_writeTimer->cancel();
+        m_writeTimer.reset();
+    }
     m_writeTimedOut = true;
     return result;
 }
