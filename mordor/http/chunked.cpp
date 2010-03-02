9a65bf4a458b9517e0b2239412883c9ec1cfdae5
Cody Cutrer <cody@mozy.com>
2010-03-02 11:28:10 -0700

    Logging for HTTP::ChunkedStream

--- mordor/http/chunked.cpp
+++ mordor/http/chunked.cpp
@@ -7,10 +7,13 @@
 #include <sstream>
 #include <stdexcept>
 
+#include "mordor/log.h"
 #include "mordor/version.h"
 
 namespace Mordor {
 namespace HTTP {
+
+static Logger::ptr g_log = Log::lookup("mordor:http:chunked");
 
 InvalidChunkException::InvalidChunkException(const std::string &line,
                                            Type type)
@@ -29,8 +32,10 @@ ChunkedStream::ChunkedStream(Stream::ptr parent, bool own)
 void
 ChunkedStream::close(Stream::CloseType type)
 {
-    if (supportsWrite() && (type & WRITE))
+    if (supportsWrite() && (type & WRITE)) {
+        MORDOR_LOG_VERBOSE(g_log) << this << " writing EOF";
         parent()->write("0\r\n", 3);
+    }
     if (ownsParent())
         parent()->close(type);
 }
@@ -44,9 +49,9 @@ ChunkedStream::read(Buffer &b, size_t len)
         chunk.resize(chunk.size() - 1);
         if (!chunk.empty() && chunk[chunk.size() - 1] == '\r')
             chunk.resize(chunk.size() - 1);
-        if (!chunk.empty()) {
+        MORDOR_LOG_TRACE(g_log) << this << " read CRLF '" << chunk << "'";
+        if (!chunk.empty())
             MORDOR_THROW_EXCEPTION(InvalidChunkException(chunk, InvalidChunkException::FOOTER));
-        }
         m_nextChunk = ~0;
     }
     if (m_nextChunk == ~0ull) {
@@ -55,20 +60,20 @@ ChunkedStream::read(Buffer &b, size_t len)
         chunk.resize(chunk.size() - 1);
         if (!chunk.empty() && chunk[chunk.size() - 1] == '\r')
             chunk.resize(chunk.size() - 1);
+        MORDOR_LOG_DEBUG(g_log) << this << " read chunk header '" << chunk
+            << "'";
         char *end;
         m_nextChunk = strtoull(chunk.c_str(), &end, 16);
-        if (end == chunk.c_str()) {
+        if (end == chunk.c_str())
             MORDOR_THROW_EXCEPTION(InvalidChunkException(chunk, InvalidChunkException::HEADER));
-        }
     }
     if (m_nextChunk == 0)
         return 0;
     size_t toRead = (size_t)std::min<unsigned long long>(len, m_nextChunk);
     size_t result = parent()->read(b, toRead);
     m_nextChunk -= result;
-    if (m_nextChunk == 0) {
+    if (m_nextChunk == 0)
         m_nextChunk = ~0ull - 1;
-    }
     return result;
 }
 
@@ -78,6 +83,7 @@ ChunkedStream::write(const Buffer &b, size_t len)
     std::ostringstream os;
     os << std::hex << len << "\r\n";
     std::string str = os.str();
+    MORDOR_LOG_DEBUG(g_log) << this << " writing chunk header " << str;
     parent()->write(str.c_str(), str.size());
     Buffer copy;
     copy.copyIn(b, len);
