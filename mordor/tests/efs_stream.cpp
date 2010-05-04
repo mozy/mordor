809fe6efe97855c8f31726419ffcb1336c65361e
Jeremy Stanley <jeremys@decho.com>
2010-05-04 15:07:31 -0600

    fix EFSStream::seek() and add unit test
    don't set "-2/skip the rest of the file" flag if we've already
    read to eof, and also be sure to unset that flag when we're done.
    (cherry picked from commit c27fb14e4350581310491afa45aad1dae38ce963)
    
    Change-Id: Ic94d05dc5121a96b6103f231eae14ec1c24c2913
    Reviewed-on: https://gerrit.dechocorp.com/6944
    Reviewed-by: Cody Cutrer <cody@mozy.com>

--- mordor/tests/efs_stream.cpp
+++ mordor/tests/efs_stream.cpp
@@ -7,6 +7,9 @@
 #include "mordor/fiber.h"
 #include "mordor/streams/file.h"
 #include "mordor/streams/efs.h"
+#include "mordor/streams/transfer.h"
+#include "mordor/streams/random.h"
+#include "mordor/streams/memory.h"
 #include "mordor/test/test.h"
 
 using namespace Mordor;
@@ -39,6 +42,58 @@ MORDOR_UNITTEST(EFSStream, basic)
     }
     DeleteFileW(L"dummy.efs");
     DeleteFileW(L"dummy2.efs");
+}
+
+MORDOR_UNITTEST(EFSStream, seek)
+{
+    {
+        // create an EFS'd file with random content
+        {
+            RandomStream r;
+            FileStream out("dummy3.efs", FileStream::WRITE, FileStream::OVERWRITE_OR_CREATE);
+            transferStream(r, out, 100000);
+        }
+        if (!EncryptFileW(L"dummy3.efs"))
+            MORDOR_THROW_EXCEPTION_FROM_LAST_ERROR_API("EncryptFileW");
+
+        // now open for reading
+        EFSStream efsr("dummy3.efs", true);
+
+        // read the file serially to a MemoryStream
+        MemoryStream control;
+        transferStream(efsr, control);
+
+        // now seek back to various offsets and ensure the data read is correct
+        long long seek_offsets[] = {
+            0,       // eof to bof
+            50000,   // mid-file forward
+            12345,   // mid-file backward
+            0,       // mid-file to bof
+            99999,   // forward, truncated read
+            100000,  // eof
+        };
+        size_t num_tests = sizeof(seek_offsets) / sizeof(seek_offsets[0]);
+        for(size_t i = 0; i < num_tests; ++i)
+        {
+            Buffer control_buf;
+            control.seek(seek_offsets[i]);
+            control.read(control_buf, 1000);
+
+            Buffer test_buf;
+            efsr.seek(seek_offsets[i]);
+            // EFSStream is known to return partial reads; that is by design
+            size_t to_read = 1000;
+            do {
+                to_read -= efsr.read(test_buf, to_read);
+            } while(to_read > 0);
+
+            MORDOR_TEST_ASSERT_EQUAL(control.tell(), efsr.tell());
+            MORDOR_TEST_ASSERT_EQUAL(control_buf.readAvailable(), test_buf.readAvailable());
+            MORDOR_TEST_ASSERT(control_buf == test_buf);
+        }
+    }
+
+    DeleteFileW(L"dummy3.efs");
 }
 
 #endif
