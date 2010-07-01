50fd099565146dd2632970563c8f1f4961f44c7b
Cody Cutrer <cody@mozy.com>
2010-07-01 13:20:32 -0600

    ThrottleStream robustness
     * Document (and explicitly implement) that either 0 or ~0u indicates
       no throttle
     * Never sleep for longer than a tenth of a second
     * Document that because of the above, practical lower bound is 800bps
       (1 byte every tenth of a second)
    
    Change-Id: I2a432b3021f40a0799b90ca3cbd3e5e77ccd5b34
    Reviewed-on: https://gerrit.dechocorp.com/8528
    Reviewed-by: Jeremy Stanley <jeremys@decho.com>
    Reviewed-by: Russ Simons <russs@mozy.com>

--- mordor/streams/throttle.cpp
+++ mordor/streams/throttle.cpp
@@ -15,16 +15,22 @@ static Logger::ptr g_log = Log::lookup("mordor:streams:throttle");
 size_t
 ThrottleStream::read(Buffer &b, size_t len)
 {
+    MORDOR_ASSERT(len != 0);
     unsigned int throttle = m_dg();
+    if (throttle == 0 || throttle == ~0u) {
+        m_read = 0;
+        return parent()->write(b, len);
+    }
     unsigned long long now = TimerManager::now();
-    unsigned long long minTime
-        = throttle ? 1000000ull * (m_read * 8) / throttle : 0;
+    unsigned long long minTime = 1000000ull * (m_read * 8) / throttle;
     unsigned long long actualTime = (now - m_readTimestamp);
     MORDOR_LOG_DEBUG(g_log) << this << " read " << m_read << "B throttle "
         << throttle << "bps now " << now << "us last " << m_readTimestamp
         << "us min" << minTime << " us actual " << actualTime << "us";
     if (actualTime < minTime) {
         unsigned long long sleepTime = minTime - actualTime;
+        // Never sleep for longer than a tenth of a second
+        sleepTime = std::max(100000ull, sleepTime);
         if (m_timerManager)
             sleep(*m_timerManager, sleepTime);
         else
@@ -33,26 +39,32 @@ ThrottleStream::read(Buffer &b, size_t len)
     } else {
         m_readTimestamp = now;
     }
-    // Aim for no more than a 10th of a second
+    // Aim for no more than a tenth of a second's worth of data
     len = std::min<size_t>(throttle / 8 / 10, len);
-    size_t result = parent()->read(b, len);
-    m_read = result;
-    return result;
+    if (len == 0)
+        len = 1;
+    return m_read = parent()->read(b, len);
 }
 
 size_t
 ThrottleStream::write(const Buffer &b, size_t len)
 {
+    MORDOR_ASSERT(len != 0);
     unsigned int throttle = m_dg();
+    if (throttle == 0 || throttle == ~0u) {
+        m_written = 0;
+        return parent()->write(b, len);
+    }
     unsigned long long now = TimerManager::now();
-    unsigned long long minTime =
-        throttle ? 1000000ull * (m_written * 8) / throttle : 0;
+    unsigned long long minTime = 1000000ull * (m_written * 8) / throttle;
     unsigned long long actualTime = (now - m_writeTimestamp);
     MORDOR_LOG_DEBUG(g_log) << this << " write " << m_written << "B throttle "
         << throttle << "bps now " << now << "us last " << m_writeTimestamp
         << "us min" << minTime << " us actual " << actualTime << "us";
     if (actualTime < minTime) {
         unsigned long long sleepTime = minTime - actualTime;
+        // Never sleep for longer than a tenth of a second
+        sleepTime = std::max(100000ull, sleepTime);
         if (m_timerManager)
             sleep(*m_timerManager, sleepTime);
         else
@@ -61,11 +73,11 @@ ThrottleStream::write(const Buffer &b, size_t len)
     } else {
         m_writeTimestamp = now;
     }
-    // Aim for no more than a 10th of a second
+    // Aim for no more than a tenth of a second's worth of data
     len = std::min<size_t>(throttle / 8 / 10, len);
-    size_t result = parent()->write(b, len);
-    m_written = result;
-    return result;
+    if (len == 0)
+        len = 1;
+    return m_written = parent()->write(b, len);
 }
 
 }
