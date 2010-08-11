4d03cd4723cd240ca27fe6087f2f723fcee42457
Cody Cutrer <cody@mozy.com>
2010-08-11 13:03:05 -0600

    Reduce duplicated code
    Change-Id: I8e61e7dfb9510871aec03224e4e6eb5f76cf14ba
    Reviewed-on: https://gerrit.dechocorp.com/9782
    Reviewed-by: Punky Brewster (labs admin) <punkyb@example.com>
    Reviewed-by: Jeremy Stanley <jeremy@mozy.com>

--- mordor/socket.cpp
+++ mordor/socket.cpp
@@ -334,14 +334,7 @@ Socket::bind(const Address &addr)
 void
 Socket::bind(Address::ptr addr)
 {
-    MORDOR_ASSERT(addr->family() == m_family);
-    if (::bind(m_sock, addr->name(), addr->nameLen())) {
-        error_t error = lastError();
-        MORDOR_LOG_ERROR(g_log) << this << " bind(" << m_sock << ", " << *addr
-            << "): (" << error << ")";
-        MORDOR_THROW_EXCEPTION_FROM_ERROR_API(error, "bind");
-    }
-    MORDOR_LOG_DEBUG(g_log) << this << " bind(" << m_sock << ", " << *addr << ")";
+    bind(*addr);
     m_localAddress = addr;
 }
 
