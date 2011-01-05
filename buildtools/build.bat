8ee9b59388ca032328a47600ca3cf41b6e87b23d
Cody Cutrer <cody@mozy.com>
2011-01-05 15:06:23 -0700

    Allow build.bat to use vc9 or vc10
    Change-Id: Iab8ccef1c6b2f19c13a490e079fff9791f6dc31b
    Reviewed-on: https://gerrit.dechocorp.com/14798
    Reviewed-by: Jeremy Stanley <jeremy@mozy.com>

--- buildtools/build.bat
+++ buildtools/build.bat
@@ -12,7 +12,46 @@
   )
 )
 
-MSBuild /maxcpucount -P:configuration=%CONFIG% -P:platform=%PLATFORM% mordor2010.sln
+@IF "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" (
+  SET X64PLATFORM=amd64
+  SET REG_BINARY=%WINDIR%\SysWOW64\REG
+  SET REG_FLAGS=/reg:64
+) ELSE (
+  SET X64PLATFORM=x86_amd64
+  SET REG_BINARY=REG
+)
+
+@IF "%PLATFORM%" EQU "Win32" (
+  SET VCVARSPLATFORM=x86
+) ELSE IF "%PLATFORM%" EQU "x64" (
+  SET VCVARSPLATFORM=%X64PLATFORM%
+)
+
+@SET LaunchVCVars=
+@IF NOT DEFINED CXX (
+  FOR /F "usebackq tokens=2*" %%A IN (`%REG_BINARY% QUERY "HKLM\SOFTWARE\Microsoft\VisualStudio\10.0\Setup\VC" /v "ProductDir" 2^> NUL`) DO @CALL SET LaunchVCVars=%%~dpBvcvarsall.bat
+  SET CXX=vc10
+  IF NOT DEFINED LaunchVCVars (
+    FOR /F "usebackq tokens=2*" %%A IN (`%REG_BINARY% QUERY "HKLM\SOFTWARE\Microsoft\VisualStudio\9.0\Setup\VC" /v "ProductDir" 2^> NUL`) DO @CALL SET LaunchVCVars=%%~dpBvcvarsall.bat
+    SET CXX=vc9
+  )
+) ELSE IF "%CXX%" EQU "vc10" (
+  FOR /F "usebackq tokens=2*" %%A IN (`%REG_BINARY% QUERY "HKLM\SOFTWARE\Microsoft\VisualStudio\10.0\Setup\VC" /v "ProductDir" 2^> NUL`) DO @CALL SET LaunchVCVars=%%~dpBvcvarsall.bat
+) ELSE IF "%CXX%" EQU "vc9" (
+  FOR /F "usebackq tokens=2*" %%A IN (`%REG_BINARY% QUERY "HKLM\SOFTWARE\Microsoft\VisualStudio\9.0\Setup\VC" /v "ProductDir" 2^> NUL`) DO @CALL SET LaunchVCVars=%%~dpBvcvarsall.bat
+)
+
+@IF NOT DEFINED LaunchVCVars (
+  @ECHO Visual Studio not found!
+  EXIT /B 1
+)
+
+@CALL "%LaunchVCVars%" %VCVARSPLATFORM%
+IF "%CXX%" EQU "vc9" (
+  devenv mordor.sln /Build "%CONFIG%|%PLATFORM%"
+) ELSE IF "%CXX%" EQU "vc10" (
+  MSBuild /maxcpucount -P:configuration=%CONFIG% -P:platform=%PLATFORM% mordor2010.sln
+)
 @IF ERRORLEVEL 1 EXIT /B
 
 MKDIR packages
