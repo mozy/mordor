4d95ac9bac96d17e0df7a06191c0d17e0887236d
Zhaoding <zhaodingw@mozy.com>
2011-10-24 15:55:55 +0800

    fixes #45138 - Support OAuth2.0 in mordor
    Added a request broker for OAuth2.0.
    
    Change-Id: I726d83f90286c8048ad956c60ccdfc43e239ecb5
    Reviewed-on: https://gerrit.dechocorp.com/25136
    Reviewed-by: Jenkins <hudsonbuild@mozy.com>
    Reviewed-by: Russ Simons <russs@mozy.com>
    Reviewed-by: Andrew Skowronski <andrews@mozy.com>
    Reviewed-by: Henry Chen <henryc@mozy.com>

---
+++ mordor/http/oauth2.h
@@ -0,0 +1,40 @@
+#ifndef __MORDOR_HTTP_OAUTH2_H__
+#define __MORDOR_HTTP_OAUTH2_H__
+// Copyright (c) 2011 - Mozy, Inc.
+
+#include <boost/function.hpp>
+
+#include "broker.h"
+
+namespace Mordor {
+namespace HTTP {
+namespace OAuth2 {
+
+    void authorize(Request &nextRequest,
+        const std::string &token);
+
+    class RequestBroker : public RequestBrokerFilter
+    {
+    public:
+        RequestBroker(HTTP::RequestBroker::ptr parent,
+            boost::function<bool (const URI &,
+            boost::shared_ptr<ClientRequest> /* priorRequest = ClientRequest::ptr() */,
+            std::string & /* token */,
+            size_t /* attempts */)> getCredentialsDg)
+            : RequestBrokerFilter(parent),
+            m_getCredentialsDg(getCredentialsDg)
+        {}
+
+        boost::shared_ptr<ClientRequest> request(Request &requestHeaders,
+            bool forceNewConnection = false,
+            boost::function<void (boost::shared_ptr<ClientRequest>)> bodyDg = NULL);
+
+    private:
+        boost::function<bool (const URI &, boost::shared_ptr<ClientRequest>, std::string &,
+            size_t)> m_getCredentialsDg;
+    };
+
+}}}
+
+#endif // __MORDOR_HTTP_OAUTH2_H__
+
