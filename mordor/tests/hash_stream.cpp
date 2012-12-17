1133d536bfa29a1db9d0504341ff27cf6e711329
Kevin Cai <kevinc@mozy.com>
2012-12-17 15:16:28 +0800

    add HashStream unit tests
    Change-Id: I778125681ed352f715786bdb432298cc05e6385f
    Signed-off-by: Kevin Cai <kevinc@mozy.com>
    Reviewed-on: https://gerrit.dechocorp.com/44623
    Reviewed-by: Kefu Chai <kchai@vmware.com>
    Reviewed-by: Jenkins <hudsonbuild@mozy.com>
    Reviewed-by: Russ Simons <russs@mozy.com>
    Reviewed-by: Andrew Skowronski <askowronski@vmware.com>
    Reviewed-by: Pancho Avila <panchoa@mozy.com>

--- mordor/tests/hash_stream.cpp
+++ mordor/tests/hash_stream.cpp
@@ -7,7 +7,88 @@
 using namespace Mordor;
 using namespace Mordor::Test;
 
+MORDOR_UNITTEST(MD5Stream, empty)
+{
+    HashStream::ptr hashStream(new MD5Stream(NullStream::get_ptr()));
+    MORDOR_TEST_ASSERT_EQUAL(hashStream->hashSize(), (size_t)MD5_DIGEST_LENGTH);
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("d41d8cd98f00b204e9800998ecf8427e"));
+}
+
+MORDOR_UNITTEST(MD5Stream, knownMD5)
+{
+    HashStream::ptr hashStream(new MD5Stream(NullStream::get_ptr()));
+    std::string data("The quick brown fox jumps over the lazy dog");
+    hashStream->write(data, data.size());
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("9e107d9d372bb6826bd81d3542a419d6"));
+}
+
+
+#ifndef OPENSSL_NO_SHA0
+MORDOR_UNITTEST(SHA0Stream, empty)
+{
+    HashStream::ptr hashStream(new SHA0Stream(NullStream::get_ptr()));
+    MORDOR_TEST_ASSERT_EQUAL(hashStream->hashSize(), (size_t)SHA_DIGEST_LENGTH);
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("f96cea198ad1dd5617ac084a3d92c6107708c0ef"));
+}
+
+MORDOR_UNITTEST(SHA0Stream, knownSha0)
+{
+    HashStream::ptr hashStream(new SHA0Stream(NullStream::get_ptr()));
+    std::string data("The quick brown fox jumps over the lazy dog");
+    hashStream->write(data, data.size());
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("b03b401ba92d77666221e843feebf8c561cea5f7"));
+}
+#endif
+
+#ifndef OPENSSL_NO_SHA1
+MORDOR_UNITTEST(SHA1Stream, empty)
+{
+    HashStream::ptr hashStream(new SHA1Stream(NullStream::get_ptr()));
+    MORDOR_TEST_ASSERT_EQUAL(hashStream->hashSize(), (size_t)SHA_DIGEST_LENGTH);
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("da39a3ee5e6b4b0d3255bfef95601890afd80709"));
+}
+
+MORDOR_UNITTEST(SHA1Stream, knownSha1)
+{
+    HashStream::ptr hashStream(new SHA1Stream(NullStream::get_ptr()));
+    std::string data("The quick brown fox jumps over the lazy dog");
+    hashStream->write(data, data.size());
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12"));
+}
+#endif
+
 #ifndef OPENSSL_NO_SHA256
+MORDOR_UNITTEST(SHA224Stream, empty)
+{
+    HashStream::ptr hashStream(new SHA224Stream(NullStream::get_ptr()));
+    MORDOR_TEST_ASSERT_EQUAL(hashStream->hashSize(), (size_t)SHA224_DIGEST_LENGTH);
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f"));
+}
+
+MORDOR_UNITTEST(SHA224Stream, knownSha224)
+{
+    HashStream::ptr hashStream(new SHA224Stream(NullStream::get_ptr()));
+    std::string data("The quick brown fox jumps over the lazy dog");
+    hashStream->write(data, data.size());
+    std::string hexHash = hexstringFromData(hashStream->hash());
+    MORDOR_TEST_ASSERT_EQUAL(hexHash,
+        std::string("730e109bd7a8a32b1cb9d9a09aa2325d2430587ddbc0c38bad911525"));
+}
+
 MORDOR_UNITTEST(SHA256Stream, empty)
 {
     HashStream::ptr hashStream(new SHA256Stream(NullStream::get_ptr()));
