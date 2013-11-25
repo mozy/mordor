0f224402e3bdf46ab1c095b3e8ae01c89aca79ae
Xiangdong Lin <xiangdongl@mozy.com>
2013-11-25 19:04:20 +0800

    refs #111616: update for boost 1.55
    * add boost_system to resolve linking issues
    * pass args to configure from build.sh
    
    Change-Id: I0855528b9fdccdaa3f661abf67abf034b62f86b2
    Signed-off-by: Xiangdong Lin <xiangdongl@mozy.com>
    Reviewed-on: https://gerrit.dechocorp.com/60235
    Reviewed-by: Kefu Chai <chaik@mozy.com>
    Reviewed-by: Jenkins <hudsonbuild@mozy.com>
    Reviewed-by: Kevin Cai <kevinc@mozy.com>

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -12,11 +12,14 @@ fi
 if [ ! -f Makefile ]; then
     if [ "$1" = "debug" ]; then
         CXXFLAGS+=' -g -O0'
+        shift
     elif [ "$1" = "coverage" ]; then
         CXXFLAGS+=' -g -O0 -fprofile-arcs -ftest-coverage'
+        shift
     elif [ "$1" = "valgrind" ]; then
         CXXFLAGS+=' -g -O0'
         VALGRINDFLAGS=--enable-valgrind=yes
+        shift
     else
         ASSERTFLAGS=--disable-assert
     fi
@@ -24,7 +27,7 @@ if [ ! -f Makefile ]; then
         export CXXFLAGS
     fi
     which pg_config >/dev/null || POSTGRESFLAGS=--without-postgresql
-    ./configure --disable-shared $ASSERTFLAGS $POSTGRESFLAGS $VALGRINDFLAGS
+    ./configure --disable-shared $ASSERTFLAGS $POSTGRESFLAGS $VALGRINDFLAGS "$@"
 fi
 if [ $(uname) = 'Linux' ]; then
     j=$(grep processor /proc/cpuinfo | wc -l)
