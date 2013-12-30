a88499ad5077127ac59784050cc1eb120b424552
Bert Yuan <berty@mozy.com>
2013-12-30 13:39:45 +0800

    closes #115402: add idle-reconnect ability to mordor pq ConnectionPool
    Change-Id: Ie235b850d48ae13d41985d8a994b65536e9ceec3
    Signed-off-by: Bert Yuan <berty@mozy.com>
    Reviewed-on: https://gerrit.dechocorp.com/61620
    Reviewed-by: Jenkins <hudsonbuild@mozy.com>
    Reviewed-by: Kevin Cai <kevinc@mozy.com>

--- mordor/pq/connectionpool.cpp
+++ mordor/pq/connectionpool.cpp
@@ -7,6 +7,7 @@
 #include "mordor/config.h"
 #include "mordor/iomanager.h"
 #include "mordor/log.h"
+#include "mordor/timer.h"
 #include "mordor/util.h"
 
 #include "connection.h"
@@ -16,18 +17,21 @@ namespace PQ {
 
 static Logger::ptr g_logger = Log::lookup("mordor:pq:connectionpool");
 
-ConnectionPool::ConnectionPool(const std::string &conninfo, IOManager *iomanager, size_t num)
-    :m_conninfo(conninfo)
-    ,m_iomanager(iomanager)
-    ,m_mutex()
-    ,m_condition(m_mutex)
-    ,m_total(num) {
+ConnectionPool::ConnectionPool(
+    const std::string &conninfo, IOManager *iomanager,
+    size_t num, unsigned long long idleTolerance)
+    : m_conninfo(conninfo)
+    , m_iomanager(iomanager)
+    , m_mutex()
+    , m_condition(m_mutex)
+    , m_total(num)
+    , m_idleTolerance(idleTolerance) {
     FiberMutex::ScopedLock lock(m_mutex);
     try {
         for (size_t i = 0; i < m_total; i++) {
             m_freeConnections.push_back(
-                boost::shared_ptr<Connection>(
-                    new Connection(m_conninfo, m_iomanager, 0, false)));
+                std::make_pair(boost::shared_ptr<Connection>(
+                    new Connection(m_conninfo, m_iomanager, 0, false)), TimerManager::now()));
         }
     } catch(...) {
         MORDOR_LOG_ERROR(g_logger)
@@ -43,15 +47,21 @@ ConnectionPool::~ConnectionPool() {
     }
 }
 
+bool ConnectionPool::connectionExpired(unsigned long long freeTime) const
+{
+    return (m_idleTolerance != 0) && (TimerManager::now() > (freeTime + m_idleTolerance));
+}
+
 boost::shared_ptr<Connection> ConnectionPool::getConnection() {
     FiberMutex::ScopedLock lock(m_mutex);
     MORDOR_LOG_DEBUG(g_logger) << "Trying to get connection, pool size is "
                                << m_freeConnections.size();
     if (m_freeConnections.empty() && m_busyConnections.size() < m_total) {
+        MORDOR_LOG_DEBUG(g_logger) << "Trying to creat new connection";
         try {
             m_freeConnections.push_back(
-                boost::shared_ptr<Connection>(
-                    new Connection(m_conninfo, m_iomanager, 0, false)));
+                std::make_pair(boost::shared_ptr<Connection>(
+                    new Connection(m_conninfo, m_iomanager, 0, false)), TimerManager::now()));
         } catch(...) {
             MORDOR_LOG_ERROR(g_logger)
                 << boost::current_exception_diagnostic_information();
@@ -61,8 +71,12 @@ boost::shared_ptr<Connection> ConnectionPool::getConnection() {
         m_condition.wait();
     }
     MORDOR_ASSERT(!m_freeConnections.empty());
-    boost::shared_ptr<Connection> conn = *m_freeConnections.begin();
-    if (conn->status() == CONNECTION_BAD) {
+    boost::shared_ptr<Connection> conn = (*m_freeConnections.begin()).first;
+
+    // For connection which was pre-allocated in constructor but not connected,
+    // its status is CONNECTION_BAD, so we'd always connect it here.
+    if (connectionExpired((*m_freeConnections.begin()).second) || conn->status() == CONNECTION_BAD) {
+        MORDOR_LOG_WARNING(g_logger) << "Connection is expired or bad, try to re-connect";
         conn->connect();
     }
     if (conn->status() != CONNECTION_OK) {
@@ -95,22 +109,22 @@ void ConnectionPool::resize(size_t num) {
 }
 
 void ConnectionPool::releaseConnection(Connection* conn) {
-        FiberMutex::ScopedLock lock(m_mutex);
-        MORDOR_LOG_DEBUG(g_logger) << "Release connection " << conn;
-        boost::shared_ptr<Connection> c(conn, &nop<Connection*>);
-        std::list<boost::shared_ptr<Connection> >::iterator it
-            = std::find(m_busyConnections.begin(), m_busyConnections.end(), c);
-        MORDOR_ASSERT(it != m_busyConnections.end());
-        c = *it; //This line is necessary,
-        //or the pointer hold by it will be deleted after the second line
+    FiberMutex::ScopedLock lock(m_mutex);
+    MORDOR_LOG_DEBUG(g_logger) << "Release connection " << conn;
+    boost::shared_ptr<Connection> c(conn, &nop<Connection*>);
+    std::list<boost::shared_ptr<Connection> >::iterator it
+        = std::find(m_busyConnections.begin(), m_busyConnections.end(), c);
+    MORDOR_ASSERT(it != m_busyConnections.end());
+    c = *it; //This line is necessary,
+    //or the pointer hold by it will be deleted after the second line
 
-        m_busyConnections.erase(it);
-        if (m_busyConnections.size() + m_freeConnections.size() < m_total) {
-            m_freeConnections.push_front(c);
-            m_condition.signal();
-        }
-        MORDOR_LOG_DEBUG(g_logger) << "Free connections "
-                                   << m_freeConnections.size();
+    m_busyConnections.erase(it);
+    if (m_busyConnections.size() + m_freeConnections.size() < m_total) {
+        m_freeConnections.push_front(std::make_pair(c, TimerManager::now()));
+        m_condition.signal();
+    }
+    MORDOR_LOG_DEBUG(g_logger) << "Free connections "
+                               << m_freeConnections.size();
 }
 
 }}
--- mordor/pq/connectionpool.h
+++ mordor/pq/connectionpool.h
@@ -19,23 +19,38 @@ class ConnectionPool : boost::noncopyable {
 public:
     typedef boost::shared_ptr<ConnectionPool> ptr;
 
+    /// Constructor of ConnectionPool
+    /// @param conninfo       db connect string
+    /// @param iomanager      IO manager
+    /// @param size           pool size, 5 by default
+    /// @param idleTolerance  re-connect interval(in us) for idle
+    ///                           connection, disable re-connect by setting it to 0
     ConnectionPool(const std::string &conninfo, IOManager *iomanager,
-        size_t size = 5);
+        size_t size = 5, unsigned long long idleTolerance = 0);
     ~ConnectionPool();
     boost::shared_ptr<Connection> getConnection();
     void resize(size_t num);
 
 private:
     void releaseConnection(Mordor::PQ::Connection* conn);
+    bool connectionExpired(unsigned long long freeTime) const;
 
 private:
     std::list<boost::shared_ptr<Mordor::PQ::Connection> > m_busyConnections;
-    std::list<boost::shared_ptr<Mordor::PQ::Connection> > m_freeConnections;
+
+    /// @note element of the free list is pair of connection and a timestamp, the
+    ///       timestamp indicates that when the connection was put into the free list.
+    //        Before re-using any connection in the free list, should use the timestamp
+    //        to calculate wheter the connection has been idle for too long, if so should
+    //        re-connect it.
+    std::list<std::pair<
+        boost::shared_ptr<Mordor::PQ::Connection>, unsigned long long> > m_freeConnections;
     std::string m_conninfo;
     IOManager *m_iomanager;
     FiberMutex m_mutex;
     FiberCondition m_condition;
     size_t m_total;
+    unsigned long long m_idleTolerance;
 };
 
 }}
