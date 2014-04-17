e32985eb157758b632fab998cf0b9cb8d20a575e
Kevin Cai <kevinc@mozy.com>
2014-04-17 04:49:03 +0000

    exclude directories from coverage report
    * remove following directories from coverage report
      - mordor/pq/tests/
      - mordor/examples/
    
    Change-Id: I0d587d6f4ebc8f2a1f3874ea6e44b04bae3af62c
    Signed-off-by: Kevin Cai <kevinc@mozy.com>
    Reviewed-on: https://gerrit.dechocorp.com/67071
    Reviewed-by: Jenkins <hudsonbuild@mozy.com>

--- buildtools/coverage.sh
+++ buildtools/coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p test-results
-gcovr -r . -e mordor/uri.cpp -e mordor/xml/xml_parser.cpp -e mordor/json.cpp -e mordor/http/http_parser.cpp -e 'mordor/test.*' -x > test-results/coverage.xml
+gcovr -r . -e mordor/uri.cpp -e mordor/xml/xml_parser.cpp -e mordor/json.cpp -e mordor/http/http_parser.cpp -e 'mordor/tests/.*' -e 'mordor/pq/tests/.*' -e 'mordor/examples/.*' -x > test-results/coverage.xml
