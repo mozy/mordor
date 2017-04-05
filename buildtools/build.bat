25effbc880e3d89d361a9754229d5e473ab7601c
Andrew Skowronski <Andrew.Skowronski@mozy.com>
2017-04-05 11:33:14 -0400

    Simplify build.bat based on kalypso improvements
    Remove confusing legacy support for compiling 64 bit from
    32 bit machines
    
    Use simpler way to find visual studio install path.
    The env variable is easier than looking in registry
    and we have found it to be correctly set on all dev and build machines.
    
    Use SETLOCAL/ENDLOCAL to more cleanly reset the environment
    after 32 bit build to prepare for 64 bit
    
    Change-Id: Id3c9f6306deb6440bafe1526a0899f5b56c06ad4
    Signed-off-by: Andrew Skowronski <Andrew.Skowronski@mozy.com>
    Reviewed-on: https://gerrit.mozy.lab.emc.com/111900
    Reviewed-by: Jenkins <hudsonbuild@mozy.com>
    Reviewed-by: Zhaoding Wu <Zhaoding.Wu@emc.com>
    Reviewed-by: Hua Sun <Hua.Sun@emc.com>

--- buildtools/build.bat
+++ buildtools/build.bat
@@ -10,21 +10,10 @@ FOR %%A IN (%*) DO (
     @IF /i "%%A" EQU "no32" SET BUILD32=0
 )
 
-@IF "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" (
-  SET X64PLATFORM=amd64
-  SET REG_BINARY=%WINDIR%\SysWOW64\REG
-  SET REG_FLAGS=/reg:64
-) ELSE (
-  SET X64PLATFORM=x86_amd64
-  SET REG_BINARY=REG
-)
+REM VS120 is Visual Studio 2013
+SET LaunchVCVars="%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
 
-@SET VSVER=12.0
-
-@SET LaunchVCVars=
-  FOR /F "usebackq tokens=2*" %%A IN (`%REG_BINARY% QUERY "HKLM\SOFTWARE\Microsoft\VisualStudio\%VSVER%\Setup\VC" /v "ProductDir" 2^> NUL`) DO @CALL SET LaunchVCVars=%%~dpBvcvarsall.bat
-
-@IF NOT DEFINED LaunchVCVars (
+@IF NOT EXIST %LaunchVCVars% (
   @ECHO Visual Studio not found!
   EXIT /B 1
 )
@@ -49,31 +38,26 @@ SET PATH=%winclientlib%\tools;%PATH%
     GOTO :build64
 )
 
-REM Remember PATH before running LaunchVCVars
-SET ROOTPATH=%PATH%
-
+SETLOCAL
 
 ECHO Building %SOLUTION% Win32
-@SET VCVARSPLATFORM=x86
 @SET PLATFORM=Win32
-@CALL "%LaunchVCVars%" %VCVARSPLATFORM%
+@CALL %LaunchVCVars% x86
 
 MSBuild /maxcpucount -P:configuration=%CONFIG% -P:platform=%PLATFORM% %SOLUTION%
 @IF ERRORLEVEL 1 EXIT /B
 
-REM Remove the values added by 32-Bit LaunchVCVars so we don't get both
-SET PATH=%ROOTPATH%
-SET LIB=
-SET LIBPATH=
-SET INCLUDE=
+REM Remove the values for PATH, LIB, etc added by 32-Bit LaunchVCVars so 
+REM that they don't interfere with the 64 bit build
+ENDLOCAL
+
 
 :build64
 
 ECHO Building %SOLUTION% x64
 
 @SET PLATFORM=x64
-@SET VCVARSPLATFORM=%X64PLATFORM%
-@CALL "%LaunchVCVars%" %VCVARSPLATFORM%
+@CALL %LaunchVCVars% AMD64
 
 MSBuild /maxcpucount -P:configuration=%CONFIG% -P:platform=%PLATFORM% %SOLUTION%
 @IF ERRORLEVEL 1 EXIT /B
