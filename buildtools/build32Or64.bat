1b4b39f98615e44344cf1a32914199ec8e6e3da7
Andrew Skowronski <Andrew.Skowronski@mozy.com>
2017-06-28 10:40:03 -0400

    Windows build - use 64-bit compilation for 64 bit build
    Cross compilation x64_amd64 tool was used instead of native 64 bit
    compiler.  The results should be same but cross compiler is potentially
    slower and might run out of memory space.
    
    The difference is visible in the builds as
    
    cross compilation
    C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\CL.exe
    
    versus
    
    64-bit
    C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\CL.exe
    
    1) Native sln fix
    
    There was a bug in the build script so the 32 bit compiler
    was doing cross compilation even when running on a 64 bit machine.
    
    And it turns out that cmake doesn't rely on the vcvarsall.bat
    when generating, so only call that for the old mechanims.
    
    2) CMake fix
    
    For CMake a PreferredToolArchitecture flag is needed
    because VS2013 defaults to the cross compiler.
    
    Reference:
    (https://connect.microsoft.com/VisualStudio/feedback/details/800059/isnativeenvironment-true-no-longer-works-on-visual-studio-2013-rc)
    
    and
    
    https://gitlab.kitware.com/cmake/cmake/issues/15622
    
    ALSO:
    prepare for script to be reused for tethys build.
    The tethys build.bat was almost identical and fixing this same bug
    in two copies of the same script seemed foolish.
    So let tethys specify ROOT and PROJECT itself, then everything should
    run.
    
    tethys-patch: 113744
    
    Testing done:
    
    Check the build output from mordor-review and mordor-review-cmake
    to make sure that x64_amd64 is gone.
    
    Change-Id: I85ff94f9a283d7df60cb2a27d13df00348fb3d74

--- buildtools/build32Or64.bat
+++ buildtools/build32Or64.bat
@@ -1,16 +1,19 @@
 IF DEFINED _ECHO ECHO ON
 SETLOCAL
 
-REM Script to compile mordor Debug or Release for either 32 or 64 bit
+REM Script to compile Debug or Release for either 32 or 64 bit
 REM This also supports performing a cmake generation for developers who want to do compilation
 REM themselves from the generated project files.
 REM This script can be invoked directly from command line or from Visual Studio via
-REM mordor_cmake.vcxproj.
+REM PROJECTNAME_cmake.vcxproj.
 REM
 REM usage examples:
 REM    build32Or64.bat Debug x64 cmakegen
 REM or
 REM    build32Or64.bat Release Win32 rebuild
+
+REM By default it build mordor, but higher level clients can use it if they specify
+REM /project and /root arguments.
 
 SET CONFIG=Release
 SET USE_CMAKE=0
@@ -21,53 +24,72 @@ SET CLEANONLY=0
 SET PLATFORM=Win32
 SET MSBUILD_TARGET=Build
 
-REM Determine absolute path of mordor no matter how this script was invoked
+SET PROJECT=Mordor
+
+REM Determine absolute path of root of the source tree no matter how this script was invoked
 SET ROOT=%~dp0%\..
 pushd %ROOT%
 SET ROOT=%CD%
 popd
 
-FOR %%A IN (%*) DO (
-    IF /i "%%A" EQU "debug" SET CONFIG=Debug
-    IF /i "%%A" EQU "coverage" SET CONFIG=Debug
-    IF /i "%%A" EQU "release" SET CONFIG=Release
+:ARGSLOOP
 
-    IF /i "%%A" EQU "x64" SET PLATFORM=x64
-    IF /i "%%A" EQU "win32" SET PLATFORM=Win32
+IF "%1" EQU "" GOTO DONEARGS
 
-    IF /i "%%A" EQU "clean" (
-        SET CLEAN=1
-        SET CLEANONLY=1
-        SET MSBUILD_TARGET=Clean
-    )
-    IF /i "%%A" EQU "rebuild" (
-        SET CLEAN=1
-        SET CLEANONLY=0
-        SET MSBUILD_TARGET=Rebuild
-    )
-
-    IF /i "%%A" EQU "nocmake" SET USE_CMAKE=0
-    IF /i "%%A" EQU "cmake" SET USE_CMAKE=1
-    IF /i "%%A" EQU "cmakegen" (
-        SET CMAKEGEN=1
-        SET USE_CMAKE=1
-    )
+IF /i "%1" EQU "/project" (
+  SET PROJECT=%2
+  SHIFT /1
+  SHIFT /1
+  GOTO ARGSLOOP
 )
 
-REM VS120 is Visual Studio 2013
-SET LaunchVCVars="%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
-
-IF NOT EXIST %LaunchVCVars% (
-  ECHO Visual Studio not found!
-  EXIT /B 1
+IF /i "%1" EQU "/root" (
+  SET ROOT=%2
+  SHIFT /1
+  SHIFT /1
+  GOTO ARGSLOOP
 )
+
+IF /i "%1" EQU "debug" SET CONFIG=Debug
+IF /i "%1" EQU "coverage" SET CONFIG=Debug
+IF /i "%1" EQU "release" SET CONFIG=Release
+
+IF /i "%1" EQU "x64" SET PLATFORM=x64
+IF /i "%1" EQU "win32" SET PLATFORM=Win32
+
+IF /i "%1" EQU "clean" (
+	SET CLEAN=1
+	SET CLEANONLY=1
+	SET MSBUILD_TARGET=Clean
+)
+
+IF /i "%1" EQU "rebuild" (
+	SET CLEAN=1
+	SET CLEANONLY=0
+	SET MSBUILD_TARGET=Rebuild
+)
+
+IF /i "%1" EQU "nocmake" SET USE_CMAKE=0
+IF /i "%1" EQU "cmake" SET USE_CMAKE=1
+
+IF /i "%1" EQU "cmakegen" (
+	SET CMAKEGEN=1
+	SET USE_CMAKE=1
+)
+	
+SHIFT /1
+GOTO ARGSLOOP
+
+:DONEARGS
+
 
 if "%winclientlib%" == "" (
     REM winclientlib variable is needed to find the third party libs
-    REM see thirdPartyPaths-win64.props
-    REM Normally it should be set as a global env variable if not set
-    REM we default to assumption that it is located in the root of the profile.
-    REM See winclientlib git project for details
+    REM and tools
+    REM Normally it should be set as a global env variable with the path
+    REM of the winclientlib git repro on the dev machine.
+    REM The convention for windows slaves is in the root
+    REM profile directory so that is the default.
 
     SET winclientlib=%USERPROFILE%\winclientlib
 )
@@ -75,18 +97,7 @@ if "%winclientlib%" == "" (
 REM Cmake is installed to winclientlib
 SET CMAKE_FULLPATH=%winclientlib%\tools\cmake-3.7.1-win64-x64\bin\cmake
 
-ECHO %MSBUILD_TARGET%ing Mordor %PLATFORM% %CONFIG%
-
-@CALL %LaunchVCVars% x86
-IF DEFINED _ECHO ECHO ON
-
-IF "%PLATFORM%" EQU "Win32" (
-    SET VCVARARG=x86
-    SET GENERATOR="Visual Studio 12 2013"
-) ELSE (
-    SET VCVARARG=AMD64
-    SET GENERATOR="Visual Studio 12 2013 Win64"
-)
+ECHO %MSBUILD_TARGET%ing %PROJECT% %PLATFORM% %CONFIG%
 
 IF "%CLEAN%" EQU "1" (
     REM For cmake case all the generated output is centralized outside of the
@@ -108,6 +119,13 @@ REM Actual output will go into Debug or Release directory inside the platform di
 IF NOT EXIST %PLATFORM% mkdir %PLATFORM%
 pushd %PLATFORM%
 
+IF "%PLATFORM%" EQU "Win32" (
+    SET GENERATOR="Visual Studio 12 2013"
+) ELSE (
+    SET GENERATOR="Visual Studio 12 2013 Win64"
+    SET PreferredToolArchitecture=x64
+)
+
 REM Generate vcxproj
 REM IF EXIST CMakeCache.txt DEL CMakeCache.txt
 %CMAKE_FULLPATH% -G %GENERATOR% %ROOT%
@@ -127,11 +145,26 @@ GOTO donebuild
 :buildsolution
 REM Old method using handmade sln file
 
+REM VS120 is Visual Studio 2013
+SET LaunchVCVars="%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
+
+IF NOT EXIST %LaunchVCVars% (
+  ECHO Visual Studio not found!
+  EXIT /B 1
+)
+
+IF "%PLATFORM%" EQU "Win32" (
+    SET VCVARARG=x86
+) ELSE (
+    SET VCVARARG=AMD64
+)
+
+@CALL %LaunchVCVars% %VCVARARG%
+IF DEFINED _ECHO ECHO ON
+
 ECHO Adding winclientlib tools to path
-REM This is based on knowledge of where happens to be installed on the build machine
-REM see winclientlib git project for details
 SET PATH=%winclientlib%\tools;%PATH%
-SET SOLUTION=Mordor.sln
+SET SOLUTION=%PROJECT%.sln
 
 MSBuild /maxcpucount -P:configuration=%CONFIG% -P:platform=%PLATFORM% %SOLUTION% /t:%MSBUILD_TARGET%
 IF ERRORLEVEL 1 EXIT /B
